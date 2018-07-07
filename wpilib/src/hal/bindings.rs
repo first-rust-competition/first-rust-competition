ec709607b06c7d8fffae00c3ca25171d67c58cba
Josh Hejna <josh.hejna@gmail.com>
2018-07-07 01:12:48 -0700

    Remove false comment

--- wpilib/src/hal/bindings.rs
+++ wpilib/src/hal/bindings.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
-#![allow(dead_code)] // prune once the lib is in a good state")
+#![allow(dead_code)]
 #![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/hal_bindings.rs"));
