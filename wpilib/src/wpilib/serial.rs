dbbed3e31fdf0f453275d77533762bff43369035
Josh Hejna <josh.hejna@gmail.com>
2018-08-25 23:18:20 -0700

    Use correct types for Serial, fix constructor

--- wpilib/src/wpilib/serial.rs
+++ wpilib/src/wpilib/serial.rs
@@ -65,16 +65,20 @@ impl SerialPort {
             port as HAL_SerialPort,
             baud_rate as i32
         ))?;
-        hal_call!(HAL_SetSerialParity(port as HAL_SerialPort, databits as i32))?;
+        hal_call!(HAL_SetSerialDataBits(
+            port as HAL_SerialPort,
+            databits as i32
+        ))?;
+        hal_call!(HAL_SetSerialParity(port as HAL_SerialPort, parity as i32))?;
         hal_call!(HAL_SetSerialStopBits(
             port as HAL_SerialPort,
             stopbits as i32
         ))?;
 
-        let serial_port = SerialPort { port };
-        serial_port.set_timeout(5.0);
-        serial_port.set_write_buf_mode(WriteBufferMode::FlushOnAcces);
-        serial_port.enable_termination('\n' as i8);
+        let mut serial_port = SerialPort { port };
+        serial_port.set_timeout(5.0)?;
+        serial_port.set_write_buf_mode(WriteBufferMode::FlushOnAcces)?;
+        serial_port.enable_termination('\n' as u8)?;
         report_usage(resource_type!(SerialPort), 0);
         Ok(serial_port)
     }
@@ -86,7 +90,7 @@ impl SerialPort {
         ))
     }
 
-    pub fn enable_termination(&mut self, terminator: i8) -> HalResult<()> {
+    pub fn enable_termination(&mut self, terminator: u8) -> HalResult<()> {
         hal_call!(HAL_EnableSerialTermination(
             self.port as HAL_SerialPort,
             terminator
@@ -97,11 +101,11 @@ impl SerialPort {
         hal_call!(HAL_DisableSerialTermination(self.port as HAL_SerialPort))
     }
 
-    pub fn get_bytes_received(&mut self) -> HalResult<u32> {
+    pub fn get_bytes_received(&mut self) -> HalResult<i32> {
         hal_call!(HAL_GetSerialBytesReceived(self.port as HAL_SerialPort))
     }
 
-    pub fn read(&mut self, buf: &mut [i8]) -> HalResult<()> {
+    pub fn read(&mut self, buf: &mut [u8]) -> HalResult<i32> {
         hal_call!(HAL_ReadSerial(
             self.port as HAL_SerialPort,
             buf.as_mut_ptr(),
@@ -109,7 +113,18 @@ impl SerialPort {
         ))
     }
 
-    pub fn write(&mut self, buf: &[i8]) -> HalResult<()> {
+    pub fn read_len(&mut self, buf: &mut [u8], len: usize) -> HalResult<i32> {
+        let len = ::std::cmp::max(len, buf.len());
+        hal_call!(HAL_ReadSerial(
+            self.port as HAL_SerialPort,
+            buf.as_mut_ptr(),
+            len as i32
+        ))
+    }
+
+    /// # Returns
+    /// Then number of bytes actually written to the buffer.
+    pub fn write(&mut self, buf: &[u8]) -> HalResult<i32> {
         hal_call!(HAL_WriteSerial(
             self.port as HAL_SerialPort,
             buf.as_ptr(),
@@ -118,10 +133,7 @@ impl SerialPort {
     }
 
     pub fn set_timeout(&mut self, seconds: f64) -> HalResult<()> {
-        hal_call!(HAL_DisableSerialTermination(
-            self.port as HAL_SerialPort,
-            seconds
-        ))
+        hal_call!(HAL_SetSerialTimeout(self.port as HAL_SerialPort, seconds))
     }
 
     pub fn set_read_buf_size(&mut self, size: u32) -> HalResult<()> {
