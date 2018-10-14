5654dec5d9d991829806835fbea830ce9094cf93
Josh Hejna <josh.hejna@gmail.com>
2018-10-14 15:26:47 -0700

    Style load-gcc-arm-headers.py properly
    * Add license notice
    * Fix shebang
    * Format according to python conventions

--- wpilib/HAL/load-gcc-arm-headers.py
+++ wpilib/HAL/load-gcc-arm-headers.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python2
+
+# This file is part of "first-rust-competition", which is free software: you
+# can redistribute it and/or modify it under the terms of the GNU General
+# Public License version 3 as published by the Free Software Foundation. See
+# <https://www.gnu.org/licenses/> for a copy.
+
 from subprocess import Popen, call, PIPE, STDOUT
 from re import search
 from os import getenv
 
 
 def main():
-    regex='(?:#include ["<]\.\.\.[">] search starts here:\n){2}((?:(?:.*?)\n)*)(?:End of search list\.)'
+    regex = '(?:#include ["<]\.\.\.[">] search starts here:\n){2}((?:(?:.*?)\n)*)(?:End of search list\.)'
 
     compiler = getenv("CXX_FRC", 'arm-frc-linux-gnueabi-g++')
     # print compiler
 
-    p = Popen([compiler, '-E', '-Wp,-v', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=False)
+    p = Popen([compiler, '-E', '-Wp,-v', '-'], stdin=PIPE,
+              stdout=PIPE, stderr=PIPE, shell=False)
     stdout, stderr = p.communicate(b"\n")
     rc = p.returncode
     include_dirs = search(regex, stderr).group(1).strip().split('\n')
