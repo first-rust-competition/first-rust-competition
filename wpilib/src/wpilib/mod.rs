2449960751f98d0703d923eea52c0dfa821aa17d
David Vo <auscompgeek@users.noreply.github.com>
2018-10-14 13:23:06 +1100

    Add basic DigitalInput
    Very barebones; lacking interrupt support.

--- wpilib/src/wpilib/mod.rs
+++ wpilib/src/wpilib/mod.rs
@@ -6,8 +6,8 @@
 // no code in this module should have any `unsafe` calls.
 // Anything needing an unsafe call should be abstracted using either `hal_call!` or something else in the `hal` module.
 
-mod digital_out;
-pub use self::digital_out::*;
+pub mod dio;
+pub use self::dio::{DigitalInput, DigitalOutput};
 
 mod robot_base;
 pub use self::robot_base::*;
