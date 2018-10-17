48250e5e9784eb34a40505066be3fbfbb42529f3
Josh Hejna <josh.hejna@gmail.com>
2018-10-17 12:07:46 -0700

    Fix makefiles
    * Run the cargo-frc and wpilib-examples makefiles instead of
    directly building
    * Only invoke publish.sh from ci

--- .ci/authorized.sh
+++ .ci/authorized.sh
@@ -4,7 +4,7 @@ set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker pull "$DOCKER_USERNAME"/frc:latest
 docker build -t "$DOCKER_USERNAME"/frc:"$TRAVIS_COMMIT" -t "$DOCKER_USERNAME"/frc:latest . --cache-from "$DOCKER_USERNAME"/frc:latest
-docker run -it -e CRATESIO_TOKEN -e TRAVIS_TAG "$DOCKER_USERNAME"/frc:"$TRAVIS_COMMIT" sh -c "cd /first-rust-competition; make all"
+docker run -it -e CRATESIO_TOKEN -e TRAVIS_TAG "$DOCKER_USERNAME"/frc:"$TRAVIS_COMMIT" sh -c "cd /first-rust-competition; make ci"
 docker images
 docker push "$DOCKER_USERNAME"/frc:"$TRAVIS_COMMIT"
 docker push "$DOCKER_USERNAME"/frc:latest
