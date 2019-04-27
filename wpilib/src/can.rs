8d92431cafdd2814157351447692df4483910812
David Vo <auscompgeek@users.noreply.github.com>
2019-04-27 12:49:00 +1000

    Document Can and add license header

--- wpilib/src/can.rs
+++ wpilib/src/can.rs
@@ -1,9 +1,19 @@
+// Copyright 2019 First Rust Competition Developers.
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Contains objects for sending and receiving frames from a device on the CAN bus.
+
 use wpilib_sys::*;
 
 pub const TEAM_MANUFACTURER: HAL_CANManufacturer::Type = HAL_CANManufacturer::HAL_CAN_Man_kTeamUse;
 pub const TEAM_DEVICE_TYPE: HAL_CANDeviceType::Type = HAL_CANDeviceType::HAL_CAN_Dev_kMiscellaneous;
 
 #[derive(Debug, Clone)]
+/// The inner return type of the various `Can` read methods.
 pub struct CanData {
     data: [u8; 8],
     length: i32,
@@ -34,6 +44,9 @@ impl CanData {
 
 /// A device on the CAN bus that conforms to the standard CAN spec
 /// and uses the FRC CAN bus addressing.
+///
+/// No packets that can be sent gets blocked by the roboRIO,
+/// so all methods work identically in all robot modes.
 #[derive(Debug)]
 pub struct Can {
     handle: HAL_CANHandle,
@@ -43,6 +56,10 @@ impl Can {
     pub const TEAM_MANUFACTURER: HAL_CANManufacturer::Type = TEAM_MANUFACTURER;
     pub const TEAM_DEVICE_TYPE: HAL_CANDeviceType::Type = TEAM_DEVICE_TYPE;
 
+    /// Create an interface to a CAN device with the specified device ID,
+    /// manufacturer, and device type.
+    ///
+    /// The device ID is 6 bits, the manufacturer is 8 bits, and the device type is 5 bits.
     pub fn new(
         device_id: i32,
         device_manufacturer: HAL_CANManufacturer::Type,
@@ -57,10 +74,16 @@ impl Can {
         Ok(Can { handle })
     }
 
+    /// Create an interface to a CAN device with the specified device ID.
+    /// This uses the team manufacturer and device types.
+    ///
+    /// The device ID is 6 bits (0-63).
     pub fn with_team_device(device_id: i32) -> HalResult<Self> {
         Can::new(device_id, TEAM_MANUFACTURER, TEAM_DEVICE_TYPE)
     }
 
+    /// Write a packet to the CAN device with a specific ID.
+    /// This ID is 10 bits.
     pub fn write_packet(&self, data: &[u8], api_id: i32) -> HalResult<()> {
         hal_call!(HAL_WriteCANPacket(
             self.handle,
@@ -70,6 +93,10 @@ impl Can {
         ))
     }
 
+    /// Write a repeating packet to the CAN device with a specific ID.
+    /// This ID is 10 bits.
+    ///
+    /// The roboRIO will automatically repeat the packet at the specified interval.
     pub fn write_packet_repeating(
         &mut self,
         data: &[u8],
@@ -85,10 +112,17 @@ impl Can {
         ))
     }
 
+    /// Stop a repeating packet with a specific ID.
+    /// This ID is 10 bits.
     pub fn stop_packet_repeating(&mut self, api_id: i32) -> HalResult<()> {
         hal_call!(HAL_StopCANPacketRepeating(self.handle, api_id))
     }
 
+    /// Read a new CAN packet.
+    ///
+    /// This will only return `Some` once per packet received.
+    ///
+    /// Any calls made before receiving another packet will return `None`.
     pub fn read_packet_new(&self, api_id: i32) -> HalResult<Option<CanData>> {
         let mut data = [0; 8];
         let mut length = 0;
@@ -111,6 +145,11 @@ impl Can {
         }
     }
 
+    /// Read a CAN packet.
+    ///
+    /// This will always return the last packet received, without accounting for packet age.
+    ///
+    /// Returns `None` if no packet has ever been received with the given ID.
     pub fn read_packet_latest(&self, api_id: i32) -> HalResult<Option<CanData>> {
         let mut data = [0; 8];
         let mut length = 0;
@@ -133,6 +172,12 @@ impl Can {
         }
     }
 
+    /// Read a CAN packet.
+    ///
+    /// This will return the last packet received
+    /// until the packet is older than the requested timeout.
+    ///
+    /// Returns `None` if no packet has been received within the timeout period.
     pub fn read_packet_timeout(&self, api_id: i32, timeout_ms: i32) -> HalResult<Option<CanData>> {
         let mut data = [0; 8];
         let mut length = 0;
@@ -160,6 +205,16 @@ impl Can {
         }
     }
 
+    /// Read a CAN packet.
+    ///
+    /// This will return the last packet received
+    /// until the packet is older than the requested timeout.
+    /// Returns `None` if no packet has been received within the timeout period.
+    ///
+    /// Packets are cached until the specified period has passed.
+    /// This is useful when you know the packet is sent at a regular interval.
+    /// Any intermediary calls will not result in a read from FRC NetComm.
+    /// It is not recommended to use this unless you understand the implications.
     pub fn read_periodic_packet(
         &self,
         api_id: i32,
