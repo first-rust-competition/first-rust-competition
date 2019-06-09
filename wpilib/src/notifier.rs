84bf805556ca7aaf7ba26f0e4ac91731de5737b7
Redrield <redrield@gmail.com>
2019-06-09 18:00:04 -0400

    more linter bikeshedding

--- wpilib/src/notifier.rs
+++ wpilib/src/notifier.rs
@@ -69,18 +69,13 @@ impl Notifier {
         let alarm = Arc::new(Alarm::new()?);
 
         let thread_alarm = alarm.clone();
-        thread::spawn(move || loop {
-            match thread_alarm.wait() {
-                Ok(cur_time) => {
-                    if cur_time == 0 {
-                        break;
-                    }
-
-                    handler();
-                    let _ = thread_alarm.update(cur_time + period.as_micros() as u64);
-                }
-                Err(_) => break,
+        thread::spawn(move || while let Ok(cur_time) = thread_alarm.wait() {
+            if cur_time == 0 {
+                break;
             }
+
+            handler();
+            let _ = thread_alarm.update(cur_time + period.as_micros() as u64);
         });
 
         Ok(Notifier { alarm })
