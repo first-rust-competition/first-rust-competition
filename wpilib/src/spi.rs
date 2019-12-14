91f52324dfef5d0a03e1a8ab8706b4424ee8b172
David Vo <auscompgeek@users.noreply.github.com>
2019-12-14 23:59:24 +1100

    fixup! spi: Implement embedded-hal Transfer

--- wpilib/src/spi.rs
+++ wpilib/src/spi.rs
@@ -155,7 +155,8 @@ impl embedded_hal::blocking::spi::Transfer<u8> for Spi {
     type Error = io::Error;
 
     fn transfer<'w>(&mut self, words: &'w mut [u8]) -> io::Result<&'w [u8]> {
-        let size = unsafe { self.transaction_into(words, words.as_mut_ptr()) }?;
+        let ptr = words.as_mut_ptr();
+        let size = unsafe { self.transaction_into(words, ptr) }?;
         Ok(&words[..size])
     }
 }
