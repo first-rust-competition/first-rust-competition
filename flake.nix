e8e7b06dc88c002102d057b2f9cb85c5cd61b69c
Bobbbay <abatterysingle@gmail.com>
2022-05-23 01:24:05 +0000

    feat(nix): add support for Nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nci.url = "github:yusdacra/nix-cargo-integration";
+  };
+  outputs = inputs: let
+    wpilib-toolchain = pkgs: pkgs.stdenv.mkDerivation rec {
+      name = "wpilib-cross-compiler";
+      version = "2022-1";
+
+      nativeBuildInputs = with pkgs; [
+        # Patch our binaries!
+        autoPatchelfHook
+
+        # Binary dependencies (patched during build)
+        ncurses5.dev
+        zlib.dev
+        expat.dev
+        xz.dev
+        python27Full
+      ];
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/wpilibsuite/roborio-toolchain/releases/download/v2022-1/FRC-2022-Linux-Toolchain-7.3.0.tar.gz";
+        sha256 = "sha256-snzeMC5G0RUkrt9mQSm8OsffAqeND55Ks/H+tA1merQ=";
+      };
+      sourceRoot = ".";
+
+      installPhase = ''
+        cp -r frc2022/roborio $out
+      '';
+    };
+  in inputs.nci.lib.makeOutputs {
+    root = ./.;
+    pkgsOverlays = [
+    ];
+
+    overrides.shell = common: prev: {
+      packages = prev.packages ++ (with common.pkgs; [ (wpilib-toolchain pkgs) jdk11 gcc makeWrapper ]);
+    };
+  };
+}
